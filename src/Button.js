94705cb5cc6061eaf88974509c667c07dc2dc9fc
adi501 <jc.adi101@gmail.com>
2020-01-09 21:31:31 +0530

    .

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Button = props => {
-  return <button className={"btn-" + props.theme}>{props.children}</button>;
+  return (
+    <button onClick={props.handleClick} className={"btn-" + props.theme}>
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from "./Button";
-import Container from "./Container";
-
 import "./styles.css";
 
 function App() {
+  function submitClick(e) {
+    e.preventDefault();
+  }
+  function resetClick(e) {
+    e.preventDefault();
+  }
+  function cancelClick(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
-      <Button theme="default">Click Me</Button>
-      <Button theme="primary">Submit</Button>
-      <Button theme="secondary">Notify</Button>
+      <Button theme="green" handleClick={submitClick}>
+        Submit
+      </Button>
+      <Button theme="blue" handleClick={resetClick}>
+        Reset
+      </Button>
+      <Button theme="red" handleClick={cancelClick}>
+        Cancel
+      </Button>
     </div>
   );
 }
